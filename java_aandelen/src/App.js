1ab27b4542cfc9edd4558cf4d8da8bc22c420779
Joost Lensvelt <joostlensvelt@gmail.com>
2025-02-25 14:57:10 +0100

    Ik heb een extra component gemaakt om te zien of het mogelijk is om door de API heen te gaan voor elk aparte aandeel.

--- java_aandelen/src/App.js
+++ java_aandelen/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import TabelGenerator from "./Tabel.js"
+import RendementTabel from "./Tabel_opdelen"
 
 function App() {
   return(
     <div>
+      <RendementTabel />
       <TabelGenerator />
       <p>joost</p>
     </div>
